f30777278148ec42296382330d148046402d931c
Ferdinand Bauer <ferdi@web.de>
2015-08-20 12:58:08 +0200

    ...

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,13 +3,27 @@ package com;
 import java.util.List;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.model.User;
 import com.model.UserDAO;
+
 public class Main {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		UserDAO userDAP = context.getBean(UserDAO.class);
-
+		
+		User u = new User();
+		u.setUserName("XBerndY");
+		u.setName("Bernd");
+		u.setLastName("Gutmann");
+		u.seteMail("erefr@web.de");
+		u.setPassWord("123456");
+		u.setGroupId(25);
+		
+		userDAP.save(u);
+		
+        //close resources
+        context.close(); 
 	}
 
 }
